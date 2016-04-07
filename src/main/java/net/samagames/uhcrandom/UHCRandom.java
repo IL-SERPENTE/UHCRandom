59778b958b519d15c2b2bdf97b0f256ccc5303f1
frasse_l <frasse_l@epitech.eu>
2016-04-07 17:52:00 +0200

    Changed HardObsidianModule's description

--- src/main/java/net/samagames/uhcrandom/UHCRandom.java
+++ src/main/java/net/samagames/uhcrandom/UHCRandom.java
@@ -46,7 +46,7 @@ public class UHCRandom extends JavaPlugin implements Listener
 
         /** Modules list */
         this.modules.add(new RandomModule(DiamondFlowerModule.class, null, "Les fleurs peuvent donner du diamant.", new ItemStack(Material.RED_ROSE)));
-        this.modules.add(new RandomModule(HardObsidianModule.class, null, "L'obsidienne ne peut être cassée qu'avec une pioche en diamant.", new ItemStack(Material.OBSIDIAN)));
+        this.modules.add(new RandomModule(HardObsidianModule.class, null, "L'obsidienne est plus dure à casser.", new ItemStack(Material.OBSIDIAN)));
         this.modules.add(new RandomModule(ParanoidModule.class, null, "Tout minage de diamant est annoncé aux autres joueurs.", new ItemStack(Material.DIAMOND_ORE)));
         this.modules.add(new RandomModule(RapidOresModule.class, new RapidOresModule.ConfigurationBuilder().addDefaults().build(), "Le minage vous rapporte plus de ressources.", new ItemStack(Material.GOLD_PICKAXE)));
         this.modules.add(new RandomModule(TorchThanCoalModule.class, new TorchThanCoalModule.ConfigurationBuilder().build(), "Le charbon se transforme en torches.", new ItemStack(Material.TORCH)));
