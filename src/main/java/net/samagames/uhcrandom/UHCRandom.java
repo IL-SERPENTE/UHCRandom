ebc18efd0c518000e27d94ea33d48be332980801
BlueSlime <jeremylevilain78@orange.fr>
2017-08-24 15:52:51 +0200

    Publishing source code.

--- src/main/java/net/samagames/uhcrandom/UHCRandom.java
+++ src/main/java/net/samagames/uhcrandom/UHCRandom.java
@@ -34,6 +34,22 @@ import org.bukkit.potion.PotionEffectType;
 
 import java.util.*;
 
+/*
+ * This file is part of UHCRandom.
+ *
+ * UHCRandom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UHCRandom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UHCRandom.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class UHCRandom extends JavaPlugin implements Listener
 {
     private static final String DESCRIPTION = "La chance sera-t-elle avec vous ?";
